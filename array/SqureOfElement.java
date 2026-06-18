f5ba4505a7cfe589a52e2c6945b664c180404ad7
Pabitra Panda <pabitra200369@gmail.com>
2026-06-18 21:32:02 +0530

    array
    array operation

---
+++ array/SqureOfElement.java
@@ -0,0 +1,12 @@
+package array;
+
+public class SqureOfElement {
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4,5};
+        for(int i=0; i<a.length; i++) {
+            System.out.print(a[i] * a[i] + " ");
+            }
+        }
+    }
+
+
