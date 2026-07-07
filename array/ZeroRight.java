e064acf97d9d9b7aaa97c0a2e8e36429d9cb2806
Pabitra Panda <pabitra200369@gmail.com>
2026-07-07 10:00:25 +0530

    array
    array  operation

---
+++ array/ZeroRight.java
@@ -0,0 +1,18 @@
+package array;
+
+class ZeroRight {
+    public static void main(String[] args) {
+
+        int a[]={2,0,5,3,1,0,3,1};
+
+        for(int i=0;i<a.length;i++) {
+            if(a[i]!=0)
+                System.out.print(a[i]+" ");
+        }
+
+        for(int i=0;i<a.length;i++) {
+            if(a[i]==0)
+                System.out.print(a[i]+" ");
+        }
+    }
+}
