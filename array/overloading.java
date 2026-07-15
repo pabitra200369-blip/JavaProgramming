35db380c28b0e5ba4a054d9350525c3370a8d34c
Pabitra Panda <pabitra200369@gmail.com>
2026-07-15 07:12:27 +0530

    oops
    oops concept

---
+++ array/overloading.java
@@ -0,0 +1,19 @@
+class MathOperation {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class overloading {
+    public static void main(String[] args) {
+        MathOperation m = new MathOperation();
+
+        System.out.println(m.add(10, 20));
+        System.out.println(m.add(10, 20, 30));
+    }
+}
